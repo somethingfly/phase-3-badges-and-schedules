d2105a566173ac22615978a59f7dea8d464399e1
Alfred <somethingfly@somethingfly.com>
2023-12-10 17:13:31 -0800

    commit tree af6f978093ca4407fc3128b03ead481b85cf0475

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map {|e| badge_maker(e)}
+end
+
+def assign_rooms(array)
+  newArray = []
+  array.each_with_index {|e, i| newArray.push("Hello, #{e}! You'll be assigned to room #{i+1}!")}
+  newArray
+end
+
+def printer(array)
+  array.each {|e| puts badge_maker(e)}
+  assign_rooms(array).each {|e| puts e}
+end
